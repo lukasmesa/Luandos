fd09e3969f4aee45c7f428da685c653e1e47dc85
lukasmesa <lukasmesa@users.noreply.github.com>
2017-10-13 11:46:06 -0500

    Cambios bootstrap y diagramas de secuencia
    Cambios en el proyecto para que funcione bootstrap y diagramas de
    secuencia

--- sistemaCotizacion/db/migrate/20171012211121_create_activities.rb
+++ sistemaCotizacion/db/migrate/20171012211121_create_activities.rb
@@ -3,6 +3,7 @@ class CreateActivities < ActiveRecord::Migration[5.1]
     create_table :activities do |t|
       t.string :name, limit: 50
       t.string :description, limit: 200
+      t.integer :value
       t.references :activity_type, foreign_key: true
 
       t.timestamps
