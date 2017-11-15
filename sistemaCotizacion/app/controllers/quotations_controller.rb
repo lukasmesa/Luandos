9ed48d61c66365dcba09a3ddfebc1abbfcd5dbef
lukasmesa <lukasmesa@users.noreply.github.com>
2017-11-15 14:54:35 -0500

    Cambios BD
    Cambios BD y consulta de líneas

--- sistemaCotizacion/app/controllers/quotations_controller.rb
+++ sistemaCotizacion/app/controllers/quotations_controller.rb
@@ -11,31 +11,45 @@ class QuotationsController < ApplicationController
     @quotation = Quotation.new
   end
 
-def create
-  respond_to do |format|
-    format.html { redirect_to quotations_path }
-    format.js   {
-                  @quotation = Quotation.new(quotation_params)
-                  @quotation.save!
-                  if params.dig(:quotation, :service).to_i == 1
-                    if params.dig(:quotation, :name).eql?("Baño")
-                      @activities = Activity.where(construction_type_id: 1)
-                      @activities.each do |activity|
-                        @service = @quotation.services.create(quotation_id: @quotation.id, activity_id: activity.id)
-                      end
-                    elsif params.dig(:quotation, :name).eql?("Cocina")
-                      @activities = Activity.where(construction_type_id: 2)
-                      @activities.each do |activity|
-                        @service = @quotation.services.create(quotation_id: @quotation.id, activity_id: activity.id)
+  def create
+    respond_to do |format|
+      format.html { redirect_to quotations_path }
+      format.js   {
+                    @quotation = Quotation.new(quotation_params)
+                    @quotation.save!
+                    if params.dig(:quotation, :service).to_i == 1
+                      if params.dig(:quotation, :name).eql?("Baño")
+                        @activities = Activity.where(construction_type_id: 1)
+                        @activities.each do |activity|
+                          @service = @quotation.services.create(quotation_id: @quotation.id, activity_id: activity.id)
+                        end
+                      elsif params.dig(:quotation, :name).eql?("Cocina")
+                        @activities = Activity.where(construction_type_id: 2)
+                        @activities.each do |activity|
+                          @service = @quotation.services.create(quotation_id: @quotation.id, activity_id: activity.id)
+                        end
                       end
                     end
-                  end
-                }
-   end
-end
-
-private
-  def quotation_params
-    params.require(:quotation).permit(:name, :floor_length, :floor_width, :wall_height, :wall_width, :budget, :project_status)
+                    @lines = findLine
+                    @lines.each do |line|
+                      if params.dig(:quotation, :budget).to_i >= line.min_value and params.dig(:quotation, :budget).to_i < line.max_value
+                        @products = Product.where(product_line_id: line.id)
+                        @products.each do |product|
+                          @article = @quotation.articles.create(quotation_id: @quotation.id, product_id: product.id, quantity: 1)
+                        end
+                      end
+                    end
+                  }
+     end
   end
+
+  def findLine
+    lines = ProductLine.all
+  end
+
+  private
+    def quotation_params
+      params.require(:quotation).permit(:name, :floor_length, :floor_width, :wall_height, :wall_width, :budget, :project_status)
+    end
+
 end
--- sistemaCotizacion/app/models/activity.rb
+++ sistemaCotizacion/app/models/activity.rb
@@ -5,7 +5,7 @@ class Activity < ApplicationRecord
   has_many :quotations, through: :services
 
   validates :name, format: {with:/\A[a-zA-Z ]+\p{Latin}+\z/, message: "El nombre solo puede contener letras"}, length: { maximum: 50 }, presence: true
-  validates :description, format: { with: /\A[A-Za-z0-9-\/\.\s]+\p{Latin}+\z/, message: "El nombre solo puede contener letras, comas, puntos y guiones" }, length: { maximum: 200 }, presence: true
+  validates :description, format: { with: /\A[A-Za-z0-9\/\\\.\,\:\s\-\_]+\p{Latin}+\z/, message: "El nombre solo puede contener letras, comas, puntos y guiones" }, length: { maximum: 200 }, presence: true
   validates :quantity, numericality: { only_integer: true }, presence: true
   validates :unit_value, numericality: { only_integer: true }, presence: true
   validates :value_meter, numericality: { only_integer: true }, presence: true
