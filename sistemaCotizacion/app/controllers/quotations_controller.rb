fa2ac2698ea3bc4ec147f3bbfc3f203f561020a2
lukasmesa <lukasmesa@users.noreply.github.com>
2017-11-15 22:26:22 -0500

    Cambios cálculo de líneas
    Cambios cálculo de líneas

--- sistemaCotizacion/app/controllers/quotations_controller.rb
+++ sistemaCotizacion/app/controllers/quotations_controller.rb
@@ -17,34 +17,53 @@ class QuotationsController < ApplicationController
       format.js   {
                     @quotation = Quotation.new(quotation_params)
                     @quotation.save!
+                    @line = findLine
                     if params.dig(:quotation, :service).to_i == 1
                       if params.dig(:quotation, :name).eql?("Baño")
-                        @activities = Activity.where(construction_type_id: 1)
+                        @activities = findActivities(1)
                         @activities.each do |activity|
                           @service = @quotation.services.create(quotation_id: @quotation.id, activity_id: activity.id)
+                        end
+                        if params.dig(:quotation, :budget).to_i >= @line.min_value and params.dig(:quotation, :budget).to_i < @line.max_value
+                        @products = Product.where("product_line_id = :line_id AND construction_type_id = 1",line_id: @line.id)
+                          @products.each do |product|
+                            @article = @quotation.articles.create(quotation_id: @quotation.id, product_id: product.id, quantity: 1)
+                          end
                         end
                       elsif params.dig(:quotation, :name).eql?("Cocina")
-                        @activities = Activity.where(construction_type_id: 2)
+                        @activities = findActivities(2)
                         @activities.each do |activity|
                           @service = @quotation.services.create(quotation_id: @quotation.id, activity_id: activity.id)
                         end
-                      end
-                    end
-                    @lines = findLine
-                    @lines.each do |line|
-                      if params.dig(:quotation, :budget).to_i >= line.min_value and params.dig(:quotation, :budget).to_i < line.max_value
-                        @products = Product.where(product_line_id: line.id)
-                        @products.each do |product|
-                          @article = @quotation.articles.create(quotation_id: @quotation.id, product_id: product.id, quantity: 1)
+                        if params.dig(:quotation, :budget).to_i >= @line.min_value and params.dig(:quotation, :budget).to_i < @line.max_value
+                        @products = Product.where("product_line_id = :line_id AND construction_type_id = 2",line_id: @line.id)
+                          @products.each do |product|
+                            @article = @quotation.articles.create(quotation_id: @quotation.id, product_id: product.id, quantity: 1)
+                          end
                         end
                       end
                     end
                   }
-     end
+    end
+  end
+
+  def findActivities(construction_type)
+    if params.dig(:quotation, :project_status).eql?("Obra Negra")
+      activities = Activity.where("construction_type_id = :construction_type AND activity_type_id <> :activity_type",{construction_type: construction_type, activity_type: 2})
+      return activities
+    elsif params.dig(:quotation, :project_status).eql?("Obra Blanca")
+      activities = Activity.where(construction_type_id: construction_type)
+      return activities
+    end
   end
 
   def findLine
     lines = ProductLine.all
+    lines.each do |line|
+      if params.dig(:quotation, :budget).to_i >= line.min_value and params.dig(:quotation, :budget).to_i < line.max_value
+        return line
+      end
+    end
   end
 
   private
